dbc34afde4a28ebb8353d8e2d192f2b6fd6fe8e8
Vladimirov Stanislav <zstanislaw211@gmail.com>
2019-08-06 13:41:10 +0300

    First test Gmail API

--- src/main/java/ru/badyavina/www/confOnly.java
+++ src/main/java/ru/badyavina/www/confOnly.java
@@ -11,10 +11,10 @@ public class confOnly {
  cd C:/Users/svladimirov/eclipse-workspace/ru.badyavina.www
   
  git add . 
- git reset -- main/resources/credentials.json
+ git reset -- 'main/resources/credentials.json'
  
- git reset -- main/resources/(web)client_id.json
- git reset -- main/resources/service-key.json
+ git reset -- 'main/resources/(web)client_id.json'
+ git reset -- 'main/resources/service-key.json'
  
  git commit -m  "First test Gmail API" 
  git push -u origin master
